179a8e81b68933802f145f42f22d48be81093096
Gabriel Augendre <gabriel@augendre.info>
2023-07-27 19:25:37 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+test:
+	go test ./...
+
+lint:
+	golangci-lint run
---
+++ cmd/api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+const (
+	readTimeoutSeconds  = 5
+	writeTimeoutSeconds = 10
+)
+
+type config struct {
+	addr string
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", "127.0.0.1:4000", "API server listen addr")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         cfg.addr,
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  readTimeoutSeconds * time.Second,
+		WriteTimeout: writeTimeoutSeconds * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}

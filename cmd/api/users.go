5a07ffae436e0c4c4d1b779dc942b97449ad8594
Gabriel Augendre <gabriel@augendre.info>
2023-09-04 19:36:32 +0200

    implement user registration

---
+++ cmd/api/users.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/Crocmagnon/greenlight/internal/data"
+	"github.com/Crocmagnon/greenlight/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	validate := validator.New()
+
+	if data.ValidateUser(validate, user); !validate.Valid() {
+		app.failedValidationResponse(w, r, validate.Errors)
+		return
+	}
+
+	err = app.models.Users.Insert(user)
+
+	switch {
+	case errors.Is(err, data.ErrDuplicateEmail):
+		validate.AddError("email", "a user with this email address already exists")
+		app.failedValidationResponse(w, r, validate.Errors)
+
+		return
+	case err != nil:
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
